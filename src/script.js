b630511e7bb28af56bb99450cfd23aa38aef41f1
dieuwertjeschipper <72558565+dieuwertjeschipper@users.noreply.github.com>
2021-01-05 17:34:51 +0100

    Added js function for date and time

---
+++ src/script.js
@@ -0,0 +1,37 @@
+function displayHours(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+    return `${hours}:${minutes}`;
+}
+
+function displayDate(timestamp) {
+    let date = new Date(timestamp);
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+let hours = date.getHours();
+if (hours < 10) {
+    hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+    minutes = `0${minutes}`;
+}
+
+let day = days[date.getDay()];
+return `${day} ${hours}:${minutes}`;
+}
